f45152dcefcdfcdb47ec2dd319d68410d6f1f310
Kacper Lechicki <klechicki.code@gmail.com>
2025-02-27 20:10:13 +0100

    conf: husky 1.0.5

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { existsSync, readFileSync } from 'fs';
-import { FlatCompat } from '@eslint/eslintrc';
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+import {FlatCompat} from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,19 +9,9 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintIgnorePath = `${__dirname}/.eslintignore`;
-let ignoredPatterns = [];
-
-if (existsSync(eslintIgnorePath)) {
-  ignoredPatterns = readFileSync(eslintIgnorePath, 'utf-8')
-    .split('\n')
-    .map((line) => line.trim())
-    .filter((line) => line && !line.startsWith('#'));
-}
-
 const eslintConfig = [
   {
-    ignores: ignoredPatterns,
+    ignores: ["node_modules/", ".next/", ".idea/"],
   },
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
 ];
